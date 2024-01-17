37c65b6a52313df1a621fa7696f82fe5434c8e33
Suraj Singh <139257270+SurajTrainer@users.noreply.github.com>
2024-01-17 20:38:33 +0530

    Recursion Details..

--- recursion_practice.js
+++ recursion_practice.js
@@ -1,5 +1,89 @@
 // Recursion Practice
 
+    // Recursion in javascript
+// what is recursion
+// recursion is a process of calling itself.
+// a function that calls itself called a recursive function.
+
+// what is base case?
+// The base case is the what stop the recursion 
+// from continuing on forever
+// it act like a terminating condition
+
+
+// why recursion?
+//how to write recursive function?
+
+
+// example of calling itself
+// function test() {
+//     test();
+// }
+// test();
+
+// another example
+function countDown(n){
+for(let i = n; i> 0; i--){
+    console.log(i);
+}
+}
+// countDown(3);
+
+// recursion
+function recursionCountDown(n){
+    if(n === 0){
+        return ;
+        
+    }console.log(n);
+    recursionCountDown(n-1);
+}
+// recursionCountDown(4);
+
+
+function totalcount(n){
+let total = 0;
+for( let i = 0; i<=n; i++){
+    total += i;
+}return total;
+}
+// console.log(totalcount(4));
+
+
+// using recursion
+function recursiontotalcount(n,total = 0){
+    //base case
+    if(n ===0)
+    return total;
+return recursiontotalcount(n-1, total += n)
+}
+// console.log(recursiontotalcount(4));
+
+// another example with nested function
+const teamstructire = {
+name : 'kunal',
+    teams:[{
+        name : 'raghav',
+        team : []
+    },
+{
+    name : 'pawan',
+    team: [],
+}]
+}
+
+function getteamDetail(t){
+    // console.log(t);
+// base case
+if(t.teams.length === 0)
+return ;
+t.teams.forEach(team => {
+    // console.log(team);
+    // getteamDetail(team);
+});
+}
+getteamDetail(teamstructire);
+
+
 function suraj() {
     return neha()
 }
